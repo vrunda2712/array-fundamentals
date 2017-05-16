a70fe8e8eb835e87b8c7576535612e78052b237f
vrunda2712 <angy.v27@gmail.com>
2017-05-16 15:20:24 -0400

    exercise2 completed

--- exercise1.rb
+++ exercise1.rb
@@ -21,7 +21,7 @@ movie_info = {
 
 city_info = {
   kitchener: 422514,
-  Hamilton: 647638,
+  hamilton: 647638,
   ottawa: 860928
 }
 
@@ -32,12 +32,39 @@ name_info = {
 }
 
 #exercise 1
-puts "Array of coin is : #{coin}"
+puts "-------------------EXERCISE 1--------------------------"
+puts "1. Array of coin is : #{coin}"
 puts
-puts "first element of fav_colours array #{fav_colours[0]}"
+puts "2. first element of fav_colours array is : #{fav_colours[0]}"
 puts
-puts "Sorted list of ages #{age.sort}"
+puts "3. Sorted list of ages is : #{age.sort}"
 puts
-puts "Added a baby in age #{age<<0}"
+puts "4. Added a baby with age is : #{age<<0}"
 puts
-puts "Logan movie released in #{movie_info[:logan]} "
+puts "5. Logan movie released in : #{movie_info[:logan]} "
+
+#exercise 2
+puts
+puts "-------------------EXERCISE 2--------------------------"
+puts "1. Last element of fav_colours array is : #{fav_colours.pop}"
+puts
+puts "2. Added new city Quebec to city_info with : #{city_info[:quebec]=659545} population"
+puts
+puts "3. Reversed array of coin is : #{coin.reverse!}"
+puts "and now coin array is : #{coin}"
+puts
+puts "4. Population of Hamilton city is : #{city_info[:hamilton]}"
+puts
+
+puts "5. Printed Statements are:"
+
+artists.each do |artist|
+  puts "---I think #{artist} is great."
+end
+
+#exercise 3
+puts
+puts "-------------------EXERCISE 3--------------------------"
+puts "1. First two performing artists is #{artists.first(2)}"
+puts
+puts
