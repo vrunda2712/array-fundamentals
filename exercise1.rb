cbab82217bd0ca2507cb319b6a3ae4144cd673b8
vrunda2712 <angy.v27@gmail.com>
2017-05-16 14:25:41 -0400

    exercise1 file created

---
+++ exercise1.rb
@@ -0,0 +1,32 @@
+#array
+
+fav_colours = ["blue","green",:red]
+age = [20,22,21]
+coin = ["heads","tails","tails","heads","heads"]
+artists = ["Laurie Anderson","Franko B","Troy Banarzi"]
+
+#hash
+
+word_info = {
+  forgettery: "a faulty memory",
+  rossarian: "a person who is fond of",
+  sophistry: "a subtle"
+}
+
+movie_info = {
+  furious: 2016,
+  logan: 2017,
+  jurassicworld: 2015
+}
+
+city_info = {
+  kitchener: 422514,
+  Hamilton: 647638,
+  ottawa: 860928
+}
+
+name_info = {
+  nish: 29,
+  ash: 22,
+  nik: 20
+}
