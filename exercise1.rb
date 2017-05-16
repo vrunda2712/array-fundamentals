63859a8789bbca4a74c2f6697dfb2bd3d7398181
vrunda2712 <angy.v27@gmail.com>
2017-05-16 16:28:05 -0400

    exercise3 completed

--- exercise1.rb
+++ exercise1.rb
@@ -33,27 +33,37 @@ name_info = {
 
 #exercise 1
 puts "-------------------EXERCISE 1--------------------------"
-puts "1. Array of coin is : #{coin}"
+puts "1. Array of coin is : "
+puts "#{coin}"
 puts
-puts "2. first element of fav_colours array is : #{fav_colours[0]}"
+puts "2. first element of fav_colours array is : "
+puts "#{fav_colours[0]}"
 puts
-puts "3. Sorted list of ages is : #{age.sort}"
+puts "3. Sorted list of ages is : "
+puts "#{age.sort}"
 puts
-puts "4. Added a baby with age is : #{age<<0}"
+puts "4. Added a baby with age is : "
+puts "#{age<<0}"
 puts
-puts "5. Logan movie released in : #{movie_info[:logan]} "
+puts "5. Logan movie released in : "
+puts "#{movie_info[:logan]} "
 
 #exercise 2
 puts
 puts "-------------------EXERCISE 2--------------------------"
-puts "1. Last element of fav_colours array is : #{fav_colours.pop}"
+puts "1. Last element of fav_colours array is : "
+puts "#{fav_colours.pop}"
 puts
-puts "2. Added new city Quebec to city_info with : #{city_info[:quebec]=659545} population"
+puts "2. Added new city Quebec to city_info with "
+puts "#{city_info[:quebec]=659545} population"
 puts
-puts "3. Reversed array of coin is : #{coin.reverse!}"
-puts "and now coin array is : #{coin}"
+puts "3. Reversed array of coin is : "
+puts "#{coin.reverse!}"
+puts "and now coin array is : "
+puts "#{coin}"
 puts
-puts "4. Population of Hamilton city is : #{city_info[:hamilton]}"
+puts "4. Population of Hamilton city is : "
+puts "#{city_info[:hamilton]}"
 puts
 
 puts "5. Printed Statements are:"
@@ -65,6 +75,17 @@ end
 #exercise 3
 puts
 puts "-------------------EXERCISE 3--------------------------"
-puts "1. First two performing artists is #{artists.first(2)}"
+puts "1. First two performing artists is "
+puts "#{artists.first(2)}"
 puts
+puts "2. Printed Statements are:"
+
+info = movie_info.each do |movie,year|
+  puts "---#{movie} came out in #{year}."
+end
 puts
+puts "3. Sort and reverse of Array of ages is : "
+puts "#{age.sort!.reverse!}"
+puts
+puts "4. Added new movie into movie_info : #{movie_info[:Beauty_and_the_beast]=[1991,2017]}"
+puts " #{movie_info}"
